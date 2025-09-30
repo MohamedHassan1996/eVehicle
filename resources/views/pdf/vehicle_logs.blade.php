5c720a2459b9edd89d149e7e81ab41fc652c7bf3
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-09-30 13:48:38 +0300

    t

--- resources/views/pdf/vehicle_logs.blade.php
+++ resources/views/pdf/vehicle_logs.blade.php
@@ -7,63 +7,89 @@
         body {
             font-family: DejaVu Sans, sans-serif;
             font-size: 14px;
-            padding: 40px;
+            padding: 40px 60px;
+            color: #333;
         }
 
         .log-page {
-            max-width: 600px;
+            width: 100%;
         }
 
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-bottom: 40px;
+        .header-table {
+            width: 100%;
+            margin-bottom: 50px;
+            border: none;
+        }
+
+        .header-table td {
+            border: none;
+            padding: 0;
         }
 
         .company-name {
-            font-size: 24px;
+            font-size: 28px;
             font-weight: bold;
             color: #111;
+            line-height: 1.2;
         }
 
         .company-subtitle {
-            font-size: 16px;
+            font-size: 18px;
             font-weight: bold;
             color: #333;
         }
 
         .date {
-            font-size: 14px;
+            font-size: 15px;
+            color: #333;
+            text-align: right;
         }
 
         .divider {
             border-top: 2px dashed #ccc;
-            margin: 30px 0;
+            margin: 35px 0;
+            height: 0;
         }
 
-        .weight-row {
-            display: flex;
-            justify-content: space-between;
-            margin: 25px 0;
-            font-size: 16px;
+        .weight-table {
+            width: 100%;
+            margin: 35px 0;
+            border: none;
+        }
+
+        .weight-table td {
+            border: none;
+            padding: 15px 0;
+            font-size: 18px;
         }
 
         .weight-label {
             font-weight: bold;
             text-transform: uppercase;
+            color: #333;
+            width: 40%;
+            text-align: left;
         }
 
         .weight-value {
             font-weight: normal;
+            color: #333;
+            width: 60%;
+            text-align: right;
         }
 
-        .netto-row {
-            display: flex;
-            justify-content: space-between;
-            margin: 25px 0;
-            font-size: 16px;
+        .netto-label {
             font-weight: bold;
+            color: #333;
+            width: 40%;
+            text-align: left;
+        }
+
+        .netto-value {
+            font-weight: bold;
+            color: #333;
+            width: 60%;
+            text-align: right;
         }
 
         .page-break {
@@ -75,43 +101,50 @@
 
     @foreach ($logs as $log)
         <div class="log-page">
-            <div class="header">
-                <div>
-                    <div class="company-name">{{ $log->vehicle->company_name }}</div>
-                </div>
-                <div class="date">
-                    Date: {{ \Carbon\Carbon::parse($log->date)->format('d/m/y') }}
-                </div>
-            </div>
+            <table class="header-table">
+                <tr>
+                    <td style="width: 50%; vertical-align: top;">
+                        <div class="company-name">BBC</div>
+                        <div class="company-subtitle">Energy</div>
+                    </td>
+                    <td style="width: 50%; vertical-align: top;">
+                        <div class="date">
+                            Date: {{ \Carbon\Carbon::parse($log->date)->format('d/m/y') }}
+                        </div>
+                    </td>
+                </tr>
+            </table>
 
             <div class="divider"></div>
 
-            <div class="weight-row">
-                <span class="weight-label">TYPE</span>
-                <span class="weight-value">KG</span>
-            </div>
-
-            <div class="weight-row">
-                <span class="weight-label">PESATA</span>
-                <span class="weight-value">{{ $log->weight }}</span>
-            </div>
-
-            <div class="weight-row">
-                <span class="weight-label">TARA</span>
-                <span class="weight-value">
-                    @php
-                        $emptyWeight = $log->vehicle->lastestEmptyVehicleWeight;
-                        echo $emptyWeight;
-                    @endphp
-                </span>
-            </div>
+            <table class="weight-table">
+                <tr>
+                    <td class="weight-label">TYPE</td>
+                    <td class="weight-value">KG</td>
+                </tr>
+                <tr>
+                    <td class="weight-label">PESATA</td>
+                    <td class="weight-value">{{ $log->weight }}</td>
+                </tr>
+                <tr>
+                    <td class="weight-label">TARA</td>
+                    <td class="weight-value">
+                        @php
+                            $emptyWeight = $log->vehicle->lastestEmptyVehicleWeight;
+                            echo $emptyWeight;
+                        @endphp
+                    </td>
+                </tr>
+            </table>
 
             <div class="divider"></div>
 
-            <div class="netto-row">
-                <span>NETTO :</span>
-                <span>{{ $log->weight - $emptyWeight }} KG</span>
-            </div>
+            <table class="weight-table">
+                <tr>
+                    <td class="netto-label">NETTO :</td>
+                    <td class="netto-value">{{ $log->weight - $emptyWeight }} KG</td>
+                </tr>
+            </table>
         </div>
 
         @if (!$loop->last)
